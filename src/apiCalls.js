a7ce367756acb0cb50e70a333afdeb804035831d
nacica <mikio.yokohama@gmail.com>
2024-04-06 12:06:44 +0900

    mod

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,7 +3,8 @@ import axios from "axios";
 export const loginCall = async (user, dispatch) => {
   dispatch({ type: "LOGIN_START" });
   try {
-    const res = await axios.post("auth/login", user);
+    // const res = await axios.post("auth/login", user);
+    const res = await axios.post("https://mernsns-backend-0404-01.onrender.com/api/auth/login", user);
     console.log(res); 
     dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
   } catch (err) {
