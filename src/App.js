e23b6de500a61253f3e3e1b735ce8e4e48befeb5
nacica <mikio.yokohama@gmail.com>
2024-04-10 11:33:01 +0900

    mod

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import Post from "./components/post/Post";
 import {
   BrowserRouter as Router,
   Route,
